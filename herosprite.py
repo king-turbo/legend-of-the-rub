15c0a58c715aa1b60dcb90846df52cd8734159d5
Michael <mwalty@gmail.com>
2018-08-06 18:34:40 -0500

    added attack direction

--- herosprite.py
+++ herosprite.py
@@ -29,54 +29,70 @@ class Hero(pygame.sprite.Sprite):
         self.swordImg =[pygame.transform.scale(pygame.image.load(wpnImg.swordImg[0]), self.size),
                         pygame.transform.scale(pygame.image.load(wpnImg.swordImg[1]),self.size)]
 
-
+        self.attackDirection = ''
 
     def updateAnimation(self, left, right, up, down, mouseX, mouseY):
 
-        mouse_buff = 100
+        mouse_buff = 50
 
         if not (left or right or up or down):
             if mouseX > self.x + mouse_buff:
                 self.charImg = self.walkRightAnimation.idlePos
+                self.attackDirection = 'right'
             if mouseX < self.x - mouse_buff:
                 self.charImg = self.walkLeftAnimation.idlePos
+                self.attackDirection = 'left'
             if mouseY < self.y - mouse_buff:
                 self.charImg = self.walkUpAnimation.idlePos
+                self.attackDirection = 'up'
             if mouseY > self.y + mouse_buff:
                 self.charImg = self.walkDownAnimation.idlePos
+                self.attackDirection = 'down'
+
         if mouseX > self.x + mouse_buff and left:
             self.walkRightAnimation.update(backwards=True)
             self.charImg = self.walkRightAnimation.img()
             self.heroSpeed = self.defaultSpeed / 2
+            self.attackDirection = 'right'
         elif left:
             self.walkLeftAnimation.update(backwards=False)
             self.charImg = self.walkLeftAnimation.img()
             self.heroSpeed = self.defaultSpeed
+            self.attackDirection = 'left'
         if mouseX < self.x - mouse_buff and right:
             self.walkLeftAnimation.update(backwards=True)
             self.charImg = self.walkLeftAnimation.img()
             self.heroSpeed = self.defaultSpeed / 2
+            self.attackDirection = 'left'
         elif right:
             self.walkRightAnimation.update(backwards=False)
             self.charImg = self.walkRightAnimation.img()
             self.heroSpeed = self.defaultSpeed
+            self.attackDirection = 'right'
+
         if mouseY < self.y - mouse_buff and down:
             self.walkUpAnimation.update(backwards=True)
             self.charImg = self.walkUpAnimation.img()
             self.heroSpeed = self.defaultSpeed / 2
-        if mouseY <= self.y - mouse_buff and up:
-            self.walkUpAnimation.update(backwards=False)
-            self.charImg = self.walkUpAnimation.img()
+            self.attackDirection = 'up'
+        elif down:
+            self. walkDownAnimation.update(backwards=False)
+            self.charImg = self.walkDownAnimation.img()
             self.heroSpeed = self.defaultSpeed
+            self.attackDirection = 'down'
         if mouseY > self.y + mouse_buff and up:
             self.walkDownAnimation.update(backwards=True)
             self.charImg = self.walkDownAnimation.img()
             self.heroSpeed = self.defaultSpeed / 2
-        if mouseY > self.y + mouse_buff and down:
-            self. walkDownAnimation.update(backwards=False)
-            self.charImg = self.walkDownAnimation.img()
+            self.attackDirection = 'down'
+        elif up:
+            self.walkUpAnimation.update(backwards=False)
+            self.charImg = self.walkUpAnimation.img()
             self.heroSpeed = self.defaultSpeed
+            self.attackDirection = 'up'
 
+
+        print(self.attackDirection)
         self.charImg = pygame.transform.scale(self.charImg, self.size)
 
     def detectAttack(self,mouse1Press, mouse1Release, mouseX, mouseY, display):
